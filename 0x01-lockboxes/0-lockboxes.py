e8eb26a47ebdede50484bd69557a81e10741b18e
adiomi90 <adiomi90@outlook.com>
2024-06-05 16:13:18 +0300

    pep8 format

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-"""Script will unlock list of lists"""
+"""Script to unlock a list of lists."""
 
 
-def canUnlockAll(boxes):
-    "this code can unlock"
+def can_unlock_all(boxes):
+    """Determine if all boxes can be unlocked.
+    
+    Arguments:
+    boxes -- a list of lists where each sublist contains keys to other boxes
+
+    Returns:
+    True if all boxes can be unlocked, False otherwise.
+    """
     keys = [0]
     keys_set = set(keys)
-    [keys.append(boxKey) for key in keys
+    [
+        keys.append(boxKey) 
+        for key in keys
         for boxKey in boxes[key]
-        if boxKey not in keys_set and boxKey < len(boxes)
-        and not keys_set.add(boxKey)]
+        if boxKey not in keys_set and boxKey < len(boxes) 
+        and not keys_set.add(boxKey)
+    ]
     return len(keys_set) == len(boxes)
