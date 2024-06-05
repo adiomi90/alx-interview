809179e444cfbd1420b18dcf7a39fc8af560fcbf
adiomi90 <adiomi90@outlook.com>
2024-06-05 16:17:07 +0300

    PEP code style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,11 +3,15 @@
 
 
 def canUnlockAll(boxes):
-    "this code can unlock"
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
+
     keys = [0]
-    keys_set = set(keys)
-    [keys.append(boxKey) for key in keys
-        for boxKey in boxes[key]
-        if boxKey not in keys_set and boxKey < len(boxes)
-        and not keys_set.add(boxKey)]
-    return len(keys_set) == len(boxes)
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
