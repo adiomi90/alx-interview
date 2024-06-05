d6bb8929dcfcd71449c7ad9930532700d658715b
adiomi90 <adiomi90@outlook.com>
2024-06-05 16:16:26 +0300

    correction on PEP8 code style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""Script to unlock a list of lists."""
+"""Script will unlock list of lists"""
 
 
-def can_unlock_all(boxes):
-    """Determine if all boxes can be unlocked.
-    
-    Arguments:
-    boxes -- a list of lists where each sublist contains keys to other boxes
-
-    Returns:
-    True if all boxes can be unlocked, False otherwise.
-    """
+def canUnlockAll(boxes):
+    "this code can unlock"
     keys = [0]
     keys_set = set(keys)
-    [
-        keys.append(boxKey) 
-        for key in keys
+    [keys.append(boxKey) for key in keys
         for boxKey in boxes[key]
-        if boxKey not in keys_set and boxKey < len(boxes) 
-        and not keys_set.add(boxKey)
-    ]
+        if boxKey not in keys_set and boxKey < len(boxes)
+        and not keys_set.add(boxKey)]
     return len(keys_set) == len(boxes)
